32e8428865d16620606382064be4697a7d6a65df
amu1016 <amu.prog@gmail.com>
2021-07-12 12:15:54 +0900

    投稿時刻の日本時間への修正

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,7 +3,7 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @room = Room.find(params[:room_id])
-    @messages = @room.meassages.includes(:user)
+    @messages = @room.messages.includes(:user)
   end
 
   def create
@@ -12,7 +12,7 @@ class MessagesController < ApplicationController
     if @message.save
       redirect_to room_messages_path(@room)
     else
-      @messages = @room.meassages.includes(:user)
+      @messages = @room.messages.includes(:user)
       render :index
     end
   end
